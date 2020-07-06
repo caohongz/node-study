3c1706c84fe5fe408c815926c59f4f19894609fd
root <root@DESKTOP-42MH02L.localdomain>
2020-07-06 22:02:51 +0800

    lesson3作业盲写

--- 03/bodyparser/index1.js
+++ 03/bodyparser/index1.js
@@ -1,36 +1,31 @@
 const Koa = require("koa");
 const app = new Koa();
-// 实际不需要koa-bodyparser
 const router = require("koa-router")();
-// 暗号：二分查找
+
 app.use(require("koa-static")(__dirname + "/"));
 app.use(async (ctx, next) => {
-  console.log("body-parser......");
-
   await new Promise((reslove, reject) => {
+    const req = ctx.request.req;
     let reqData = [];
     let size = 0;
-    const req = ctx.request.req;
     req.on("data", (data) => {
       reqData.push(data);
       size += data.length;
     });
     req.on("end", function () {
-      const data = Buffer.concat(reqData, size);
-      console.log("reqData", data.toString());
-
-      ctx.request.body = data.toString();
+      ctx.request.body = reqData.toString();
       reslove();
     });
   });
   await next();
 });
+router.post("/add", async (ctx, next) => {
+  console.log("body", ctx.request.body);
 
-router.post("/add", (ctx, next) => {
-  console.log(">>>parseing ", ctx.request.body);
   ctx.body = ctx.request.body;
-  console.log("ctx.body", ctx.body);
-  next();
+  console.log(ctx.body);
+
+  await next();
 });
 app.use(router.routes());
 app.listen(3000);
