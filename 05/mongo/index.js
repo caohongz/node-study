60581878a119e2dfc7035f8cdde8b022ad27b612
root <root@DESKTOP-42MH02L.localdomain>
2020-07-11 11:28:09 +0800

    merge data 7.11

--- 05/mongo/index.js
+++ 05/mongo/index.js
@@ -1,79 +1,34 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const mongo = require("./models/db");
+// const testdata = require("./initData")
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./index.html"));
 });
+
 app.get("/api/list", async (req, res) => {
-  const { page, category, keyword } = req.query;
-  const condition = {};
-  if (category) {
-    condition.category = category;
+  // 分页查询
+  const { page } = req.query;
+  try {
+    const col = mongo.col("fruits");
+    const total = await col.find().count();
+    const fruits = await col
+      .find()
+      .skip((page - 1) * 5)
+      .limit(5)
+      .toArray();
+    res.json({ ok: 1, data: { fruits, pagination: { total, page } } });
+  } catch (error) {
+    console.log(error);
   }
-  if (keyword) {
-    condition.name = { $regex: new RegExp(keyword) };
-  }
-  const col = mongo.col("fruits");
-  const total = await col.find(condition).count();
-  const fruits = await col
-    .find(condition)
-    .skip((page - 1) * 5)
-    .limit(5)
-    .toArray();
-  res.json({
-    ok: 1,
-    data: {
-      fruits,
-      pagination: {
-        total,
-        page,
-      },
-    },
-  });
 });
+
 app.get("/api/category", async (req, res) => {
   const col = mongo.col("fruits");
   const data = await col.distinct("category");
   res.json({ ok: 1, data });
 });
+
 app.listen(3000);
-=======
-const express = require("express")
-const app = express()
-const path = require("path")
-const mongo = require("./models/db")
-// const testdata = require("./initData")
-
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve("./index.html"))
-})
-
-app.get("/api/list", async (req, res) => {
-    // 分页查询
-    const { page} = req.query
-    try {
-        const col = mongo.col("fruits")
-        const total = await col.find().count()
-        const fruits = await col
-            .find()
-            .skip((page - 1) * 5)
-            .limit(5)
-            .toArray()
-        res.json({ ok: 1, data: { fruits, pagination: { total, page } } })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-app.get("/api/category", async (req, res) => {
-    const col = mongo.col("fruits")
-    const data = await col.distinct('category')
-    res.json({ ok: 1, data })
-})
-
-app.listen(3000)
->>>>>>> 42e88e21f12424cbb8dcc1c5b470e4b56b8b1645
